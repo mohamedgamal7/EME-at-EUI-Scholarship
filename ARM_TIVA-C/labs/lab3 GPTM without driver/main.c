704efc0acade0e9143bd6aba0668d25b19f36def
Mohamed Gamal <mohamadgamal2021@gmail.com>
2023-08-29 18:31:53 +0200

    three examples for timer modes on a register level

--- ARM_TIVA-C/labs/lab3 GPTM without driver/main.c
+++ ARM_TIVA-C/labs/lab3 GPTM without driver/main.c
@@ -5,9 +5,13 @@
 #include "./LIB/STD_TYPES.h"
 
 #include "MCAL/GPIO/GPIO_interface.h"
+#include "MCAL/EXTI/EXTI_interface.h"
 
-#define PWM
+#define FCPU 16000000UL
 
+//#define EDGE_TIME
+//#define PWM
+#define PERIODIC
 
 void _delay_ms(u32 n)
 {
@@ -16,6 +20,17 @@ void _delay_ms(u32 n)
      for(j=0;j<3180;j++)
      {};
 }
+void timer0_handler()
+{
+        SET_BIT(TIMER0_ICR_R, 0);
+        TOG_BIT(GPIO_PORTF_DATA_R, 1);
+        TOG_BIT(GPIO_PORTF_DATA_R, 2);
+        TOG_BIT(GPIO_PORTF_DATA_R, 3);
+
+}
+
+
+
 
 #ifdef PWM
 
@@ -70,5 +85,147 @@ while (1)
 
 
 }
+#endif
+
+#ifdef EDGE_TIME
+
+
+
+void switch1_handler()
+{
+    if(EXTICheckIntPin(PORTF_ID,P4_POS))
+    {
+        GPIO_Write(PORTF_ID, P0, LOW); //put a low value on pin0 port f
+    }
+
+}
+f32 ticksToSecs(u32 ticks)
+{
+   return (240000000-ticks)/FCPU;
+
+}
+
+int main(void)
+{
+    // EDge time working on timer 0 A
+
+    // initialize port F pin 1,2,3 as output
+    // initialize port f pin 4 as input and pin 0 as output
+    GPIO_Init_Port(PORTF_ID, P4, P1|P2|P3|P0);
+    // write one to protf pin 0
+    GPIO_Write(PORTF_ID, P0, HIGH); //put a low value on pin0 port f
+    // init port c as input
+    GPIO_Init_Port(PORTC_ID,P4, NO_PINS);
+    // init interuppt for port f pin 4
+    EXTI_Init(PORTF_ID, P4, HIGH, IN_PORTF);
+    // set callback function
+    EXTI_Set_Callback(&switch1_handler);
+
+
+    //afsel for pin 4 port c
+    GPIO_PORTC_AFSEL_R|=(1<<4);
+    // set it as wtimer0 in prot ctl
+    GPIO_PORTC_PCTL_R|=(0x7<<4*4);
+
+
+    // send clock to wide timer 0
+    SYSCTL_RCGCWTIMER_R|=(1<<0);
+    // diable timer 0A
+    WTIMER0_CTL_R&=~(1<<0);
+    // config value selects a 32 bit configuration
+    WTIMER0_CFG_R=0x04;
+    // timer mode of timer0 A
+    // set TACMR to set it as edge time
+    WTIMER0_TAMR_R|=(1<<2);
+    // set as capture mode
+    WTIMER0_TAMR_R|=(1<<1);
+    WTIMER0_TAMR_R|=(1<<0);
+
+    // configure the type of event the timer captures
+      WTIMER0_CTL_R|=(1<<2);
+
+    // load the timer start value to count 15 sec
+    WTIMER0_TAILR_R = 240000000 ;
+    // enable the timer
+    WTIMER0_CTL_R|=(1<<0);
+
+    // variable to capture time
+    f32 Local_time_captured = 0 ;
+while (1)
+{
+    if(!GPIO_Read(PORTC_ID, P4)) // and edge happened
+    {
+        Local_time_captured =ticksToSecs(WTIMER0_TAR_R);
+
+        if (Local_time_captured<=5)
+        {
+            GPIO_Write(PORTF_ID, P1, HIGH);
+            GPIO_Write(PORTF_ID, P2, LOW);
+            GPIO_Write(PORTF_ID, P3, LOW);
+        }
+        else if (Local_time_captured<=10)
+        {
+            GPIO_Write(PORTF_ID, P1, LOW);
+            GPIO_Write(PORTF_ID, P2, HIGH);
+            GPIO_Write(PORTF_ID, P3, LOW);
+        }
+        else if (Local_time_captured<=15)
+        {
+            GPIO_Write(PORTF_ID, P1, LOW);
+            GPIO_Write(PORTF_ID, P2, LOW);
+            GPIO_Write(PORTF_ID, P3, HIGH);
+        }
+
+//        // load the timer start value to count 15 sec
+//        WTIMER0_TAILR_R = 240000000 ;
+        GPIO_Write(PORTF_ID, P0, HIGH);
+
+    }
+
+
+}
+
+
+}
+#endif
+
+
+#ifdef PERIODIC
+
+void main(void)
+{
+
+    GPIO_Init_Port(PORTF_ID, NO_PINS, P1|P2|P3);
+    // Enabling the RCC For the Timer
+        SET_BIT(SYSCTL_RCGCTIMER_R, 0);
+
+        CLR_BIT(TIMER0_CTL_R,0);
+
+        TIMER0_CFG_R=0x04;
+
+        TIMER0_TAMR_R=0x2;
+
+        CLR_BIT(TIMER0_TAMR_R,4);
+
+        TIMER0_TAILR_R=65535;
+
+        TIMER0_TAPR_R = 122;
+
+        SET_BIT(TIMER0_IMR_R,0);
+
+        SET_BIT(TIMER0_CTL_R,0);
+
+        SET_BIT(TIMER0_ICR_R,0);
+
+        SET_BIT(NVIC_EN0_R, 19);
+
+        while(1)
+        {
+
+        }
+}
+
+
+
 #endif
 
--- ARM_TIVA-C/labs/lab3 GPTM without driver/tm4c123gh6pm_startup_ccs.c
+++ ARM_TIVA-C/labs/lab3 GPTM without driver/tm4c123gh6pm_startup_ccs.c
@@ -55,6 +55,8 @@ extern uint32_t __STACK_TOP;
 //
 //*****************************************************************************
 // To be added by user
+extern void GPIOPortF_handler(void);
+extern void timer0_handler(void);
 
 //*****************************************************************************
 //
@@ -102,7 +104,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // ADC Sequence 2
     IntDefaultHandler,                      // ADC Sequence 3
     IntDefaultHandler,                      // Watchdog timer
-    IntDefaultHandler,                      // Timer 0 subtimer A
+    timer0_handler,                      // Timer 0 subtimer A
     IntDefaultHandler,                      // Timer 0 subtimer B
     IntDefaultHandler,                      // Timer 1 subtimer A
     IntDefaultHandler,                      // Timer 1 subtimer B
@@ -113,7 +115,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // Analog Comparator 2
     IntDefaultHandler,                      // System Control (PLL, OSC, BO)
     IntDefaultHandler,                      // FLASH Control
-    IntDefaultHandler,                      // GPIO Port F
+    GPIOPortF_handler,                      // GPIO Port F
     IntDefaultHandler,                      // GPIO Port G
     IntDefaultHandler,                      // GPIO Port H
     IntDefaultHandler,                      // UART2 Rx and Tx
